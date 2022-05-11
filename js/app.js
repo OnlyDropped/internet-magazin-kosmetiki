d1363a3296e7401655d5132cf3de86472d97a3fc
OnlyDropped <asheburenkov@yandex.ru>
2022-05-11 18:22:06 +0300

    added new changes

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$('.slider').slick({
+   arrows: true,
+   prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrrow-prev.svg" alt=""></button>',
+   nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrrow-next.svg" alt=""></button>',
+});
