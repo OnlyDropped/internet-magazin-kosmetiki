5739e7e835723c7a683edb7792a9ea3e90477b71
OnlyDropped <asheburenkov@yandex.ru>
2022-05-27 17:41:02 +0300

    added new changes

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ $('.slider').slick({
    arrows: true,
    prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrrow-prev.svg" alt=""></button>',
    nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrrow-next.svg" alt=""></button>',
+   responsive: [
+    {
+      breakpoint: 376,
+      settings: {
+        arrows: false,
+      }
+    }
+  ]
 });
 
 
